4e6caeaefb4d3548e912962976cc6e4b2853386b
chsantoshkumar211 <ch.santoshkumar211@gmail.com>
2019-06-19 21:39:28 +0530

    adding selection and inserion sort

--- arr_op.py
+++ arr_op.py
@@ -9,6 +9,27 @@ def bubble_sort(array):
                 array[j+1]=temp
     return array
 
+def selection_sort(array):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i+1,len(array)):
+            if array[j]<array[min_index]:
+                min_index=j
+        temp=array[min_index]
+        array[min_index]=array[i]
+        array[i]=temp
+    return array
+
+def insertion_sort(array):
+    for i in range(1,len(array)):
+        key=array[i]
+        j=i-1
+        while j>=0 and key<array[j]:
+            array[j+1]=array[j]
+            j-=1
+        array[j+1]=key
+    return array
+
 # Basic operations
 
 def maxarray(array):
